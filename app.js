533be0dd2674cd51c3809fcf23214c449a5b3842
EmmanuelEkitela <ekitela.tech@gmail.com>
2025-03-29 16:04:09 +0300

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function showMenu(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'block'
+}
+
+function hideMenu(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+// Donations alert msg
